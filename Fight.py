6a2b79b6ee1424c07147fcd154eef4870dd2a2b3
Kyle Miho <kmiho001@ucr.edu>
2017-11-10 18:05:59 -0800

    Revert "files"
    This reverts commit c249c77f2e59b05ba09aa34c16496beccd909907.

--- Fight.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-def loadStageBG(stageNum,resolution):
-    stageImage = pygame.image.load('BG\Tavern.png')
-    stageImage.convert()
-    return pygame.transform.smoothscale(stageImage,resolution)
